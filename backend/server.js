7938bbadae3dae2d1de6e823eacd0d98d3d78e6e
Nisarg2003 <112456996+Nisarg2003@users.noreply.github.com>
2024-03-02 11:17:01 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDb()
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-system-black.vercel.app",
     },
     
 });
